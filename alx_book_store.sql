4d8c88d2b4b67cad79d41aff12e15e862313e9ca
Necodine <nellexighodaro@gmail.com>
2024-07-26 10:38:22 +0100

    database file for alx bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,7 +32,7 @@ CREATE TABLE Order_Details (
 order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
 order_id INT,
 book_id INT,
-quantity DOUBLE,
+quantity DOUBLE(10,2),
 FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
